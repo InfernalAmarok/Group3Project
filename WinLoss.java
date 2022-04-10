4e89c7a78257a51cea343be8c93ab8ae01a13b61
InfernalAmarok <103280991+InfernalAmarok@users.noreply.github.com>
2022-04-10 15:44:23 -0400

    Add files via upload

--- WinLoss.java
+++ WinLoss.java
@@ -6,10 +6,16 @@ public class WinLoss {
     public void success()
     {
         //Declares that the player is a winner and prompts them if they want to retry
+        System.out.println("The hero has prevailed at the quest set for them, despite all odds! Now with The Mercurial Conqueror slain, the world is at peace, at least for the immediate future.");
+        System.out.println("Now the hero can take a rest, until they are needed for their next grand adventure.");
+        System.out.println("That being said, you may return to try the quest again and attempt a different journey.");
+        System.out.println("Do you wish to do so? (YES / NO)");
     }
 
     public void defeat()
     {
         //Declares that the player has lost and prompts them if they want to retry
+        System.out.println("Alas, the hero has suffered an untimely demise- do not be disheartened.");
+        System.out.println("You may try again, but do you wish to do so? (YES / NO)");
     }
 }
