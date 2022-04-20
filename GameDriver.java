a6c348ff6a1a8a5cb7a74f139249c49725d14819
InfernalAmarok <103280991+InfernalAmarok@users.noreply.github.com>
2022-04-20 13:43:41 -0400

    Add files via upload
    V0.2. COMBAT BETA
    
    For the sake of actually making a working game, I have chosen to disregard the majority of the classes in favor of a simple turn-based system in the GameDriver class. As it stands, you can Attack or Defend in combat, and can challenge one of three types of slime enemies: Verdant (easy), Mud (medium), or Ice (hard). They all handle relatively the same but with harder ones having beefier stats. The game ends after slaying a slime, but can be restarted or quit ad nauseum. 
    
    KNOWN BUGS:
    -The enemy still gets in its last move after its HP drops to or below 0. This is purely a visual issue and is remedied between battles.
    -Enemy HP can go into the negatives. Again, this is purely a visual issue. 
    
    -Keillor C.

--- GameDriver.java
+++ GameDriver.java
@@ -7,20 +7,20 @@ public class GameDriver {
         String test;
         String playerInput;
         int playerHP;
-        int playerAttackDamage;
+        int playerAttackDamage = 7;
         int playerBlock = 5;
         int verSlimeHP;
         int verSlimeAttackDamage = 5;
         int verSlimeBlock = 4;
-        int mudSlimeHP = 28;
+        int mudSlimeHP;
         int mudSlimeAttackDamage = 7;
         int mudSlimeBlock = 5;
-        int iceSlimeHP = 42;
+        int iceSlimeHP;
         int iceSlimeAttackDamage = 8;
         int iceSlimeBlock = 6;
 
         while(true){
-            System.out.println("Welcome to the game Project Duplicity. Do you have what it takes to make it to the peak of Mt. Eskervon and beat your nemesis?");
+            System.out.println("Welcome to the game Project Duplicity. This is a COMBAT BETA that tests the combat capabilities of the game.");
             System.out.println("Type 'exit' to quit the game, or press enter to begin the game.");
 
             test = scan.nextLine();
@@ -45,8 +45,8 @@ public class GameDriver {
                 System.out.println("You confront the Verdant Slime.");
                 verSlimeHP = 14;
                 playerHP = 100;
+                playerAttackDamage = 7;
                 while (verSlimeHP > 0) {
-                    playerAttackDamage = 7;
                     System.out.println("Verdant Slime HP: " + verSlimeHP);
                     System.out.println("Your HP: " + playerHP);
                     System.out.println("It's your turn now. Type the letter A to attack, or the letter D to defend.");
@@ -87,8 +87,88 @@ public class GameDriver {
                 System.out.println("You have defeated the Verdant Slime!");
                 test = "";
             }
+            else if(test.equals("Mud Slime")) {
+                System.out.println("You confront the Mud Slime.");
+                mudSlimeHP = 28;
+                playerHP = 100;
+                playerAttackDamage = 7;
+                while (mudSlimeHP > 0) {
+                    System.out.println("Mud Slime HP: " + mudSlimeHP);
+                    System.out.println("Your HP: " + playerHP);
+                    System.out.println("It's your turn now. Type the letter A to attack, or the letter D to defend.");
+                    playerInput = scan.nextLine();
+                    if (playerInput.equals("A")) {
+                        mudSlimeHP = mudSlimeHP - playerAttackDamage;
+                        System.out.println("You attack the Mud Slime for " + playerAttackDamage + " damage.\n" + "It has " + mudSlimeHP + " HP left.");
+                    } else if (playerInput.equals("D")) {
+                        mudSlimeAttackDamage = mudSlimeAttackDamage - playerBlock;
+                        System.out.println("You now take 5 less damage from the Mud Slime's next attack.");
+                    }
+                    System.out.println("The Mud slime makes its move: Rock Hop!");
+                    playerHP = playerHP - mudSlimeAttackDamage;
+                    System.out.println("You take " + mudSlimeAttackDamage + " damage. Your HP is now " + playerHP + ".");
+                    System.out.println("Mud Slime HP: " + mudSlimeHP);
+                    playerAttackDamage = 7;
+                    mudSlimeAttackDamage = 7;
+                    System.out.println("It's your turn now. Type the letter A to attack, or the letter D to defend.");
+                    playerInput = scan.nextLine();
+                    if (playerInput.equals("A")) {
+                        mudSlimeHP = mudSlimeHP - playerAttackDamage;
+                        System.out.println("You attack the Mud Slime for 7 damage.");
+                        System.out.println("It has " + mudSlimeHP + " HP left.");
+                    } else if (playerInput.equals("D")) {
+                        mudSlimeAttackDamage = mudSlimeAttackDamage - playerBlock;
+                        System.out.println("You now take 5 less damage from the Mud Slime's next attack.");
+                    }
+                    System.out.println("The Mud slime makes its move: Muddy Guard!");
+                    playerAttackDamage = playerAttackDamage - mudSlimeBlock;
+                    System.out.println("Your next attack is reduced by 5 damage.");
+                }
+                System.out.println("You have defeated the Mud Slime!");
+                test = "";
+            }
+            else if(test.equals("Ice Slime")) {
+                System.out.println("You confront the Ice Slime.");
+                iceSlimeHP = 42;
+                playerHP = 100;
+                playerAttackDamage = 7;
+                while (iceSlimeHP > 0) {
+                    System.out.println("Ice Slime HP: " + iceSlimeHP);
+                    System.out.println("Your HP: " + playerHP);
+                    System.out.println("It's your turn now. Type the letter A to attack, or the letter D to defend.");
+                    playerInput = scan.nextLine();
+                    if (playerInput.equals("A")) {
+                        iceSlimeHP = iceSlimeHP - playerAttackDamage;
+                        System.out.println("You attack the Ice Slime for " + playerAttackDamage + " damage.\n" + "It has " + iceSlimeHP + " HP left.");
+                    } else if (playerInput.equals("D")) {
+                        iceSlimeAttackDamage = iceSlimeAttackDamage - playerBlock;
+                        System.out.println("You now take 5 less damage from the Ice Slime's next attack.");
+                    }
+                    System.out.println("The Ice slime makes its move: Frost Jiggle!");
+                    playerHP = playerHP - iceSlimeAttackDamage;
+                    System.out.println("You take " + iceSlimeAttackDamage + " damage. Your HP is now " + playerHP + ".");
+                    System.out.println("Ice Slime HP: " + iceSlimeHP);
+                    playerAttackDamage = 7;
+                    iceSlimeAttackDamage = 8;
+                    System.out.println("It's your turn now. Type the letter A to attack, or the letter D to defend.");
+                    playerInput = scan.nextLine();
+                    if (playerInput.equals("A")) {
+                        iceSlimeHP = iceSlimeHP - playerAttackDamage;
+                        System.out.println("You attack the Ice Slime for 7 damage.");
+                        System.out.println("It has " + iceSlimeHP + " HP left.");
+                    } else if (playerInput.equals("D")) {
+                        iceSlimeAttackDamage = iceSlimeAttackDamage - playerBlock;
+                        System.out.println("You now take 5 less damage from the Ice Slime's next attack.");
+                    }
+                    System.out.println("The Ice slime makes its move: Flake Barrier!");
+                    playerAttackDamage = playerAttackDamage - iceSlimeBlock;
+                    System.out.println("Your next attack is reduced by 6 damage.");
+                }
+                System.out.println("You have defeated the Ice Slime!");
+                test = "";
+            }
             System.out.println("Play again?\nEnter yes/no: ");
-            WinLoss.playAgain();
+                WinLoss.playAgain();
         }
 
     }
